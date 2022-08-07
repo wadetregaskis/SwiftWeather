ea3ad99943c93cfcc47ef95131ddf9afa20e86d9
Wade Tregaskis <wadetregaskis@mac.com>
2022-08-07 16:13:29 -0700

    Simplified the WeatherPlatform creation.
    • Made WeatherServiceType a String [enum], not Int [enum], so that its encoded form is more meaningful and reliable.
    • Removed unnecessary manual Codable conformance for WeatherServiceType.
    • Removed unnecessary 'Undefined' enum value from WeatherServiceType.
    • Removed unnecessary factory pattern from SwiftWeather - it now just has a single static method for creating new WeatherPlatform instances, saving a bunch of indirection and unnecessary work.

--- Sources/SwiftWeather/SwiftWeather.swift
+++ Sources/SwiftWeather/SwiftWeather.swift
@@ -36,73 +36,20 @@ public enum WeatherServiceStatus {
 }
 
 /// Supported Service Types
-public enum WeatherServiceType: Int {
-    case Undefined
-    case Ambient
+public enum WeatherServiceType: String, Codable {
+    case AmbientWeather
 }
 
-extension WeatherServiceType: Codable {
-    public static func enumvValue(value: Int) -> WeatherServiceType {
-        switch value {
-        case 0 : return .Undefined
-        case 1 : return .Ambient
-        default: return .Undefined
-        }
-    }
-    
-    enum Key: CodingKey {
-        case rawValue
-    }
-    
-    enum CodingError: Error {
-        case unknownValue
-    }
-    
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: Key.self)
-        
-        let rawValue = try container.decode(Int.self, forKey: .rawValue)
-        switch rawValue {
-        case 0:
-            self = .Undefined
-        case 1:
-            self = .Ambient
-        default:
-            throw CodingError.unknownValue
-        }
-    }
-    
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: Key.self)
-        
-        switch self {
-        case .Undefined:
-            try container.encode(0, forKey: .rawValue)
-        case .Ambient:
-            try container.encode(1, forKey: .rawValue)
-        }
-    }
-}
-
-/// Weather Factory
 public class SwiftWeather {
-    public static var WeatherFactory = SwiftWeather()
-    
-    open class func shared() -> SwiftWeather {
-        return WeatherFactory
-    }
-    
     /// Initialize the desired service using a platform model
     /// - Parameters:
-    ///   - weatherServiceType: desired weather service
+    ///   - weatherServiceType: desired weather service.
     ///   - apiKeys: key(s) that are required to initialize the service; it's up to the resulting service to handle the order and # of keys.
-    /// - Returns: a fully formed weather platform
-    public func getService(weatherServiceType: WeatherServiceType, apiKeys: [String]) -> WeatherPlatform? {
+    /// - Returns: A new weather platform of the requested type.
+    public static func create(weatherServiceType: WeatherServiceType, apiKeys: [String]) -> WeatherPlatform {
         switch weatherServiceType {
-        case .Ambient:
+        case .AmbientWeather:
             return AmbientWeather(apiKeys: apiKeys)
-        case .Undefined:
-            return nil
         }
     }
 }
