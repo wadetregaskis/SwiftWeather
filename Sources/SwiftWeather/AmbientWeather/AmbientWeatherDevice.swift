805ffc892df9cd99775a6ff7b774aa9e830b9205
Wade Tregaskis <wadetregaskis@mac.com>
2022-08-09 18:57:44 -0700

    Removed "lastData" from AmbientWeatherDevice.
    Part of the broad move away from storing state in all these objects - they're fundamentally REST API wrappers and it's better if the user of this library decides where data is stored (if it's persisted at all) - it's actually easier that way re. integrating with SwiftUI, for example.

--- Sources/SwiftWeather/AmbientWeather/AmbientWeatherDevice.swift
+++ Sources/SwiftWeather/AmbientWeather/AmbientWeatherDevice.swift
@@ -9,8 +9,7 @@ import Foundation
 open class AmbientWeatherDevice: WeatherDevice {
     private let info: AmbientWeatherStationInfo?
     private let macAddress: WeatherDeviceID
-    public let lastData: AmbientWeatherStationData?
-    
+
     enum CodingKeys: String, CodingKey {
         case macAddress = "macAddress"
         case lastData = "lastData"
@@ -19,59 +18,6 @@ open class AmbientWeatherDevice: WeatherDevice {
 
     internal(set) public var platform: WeatherPlatform
 
-    /// Return the timestamp from lastData, as a NSDate in UTC
-    public var timestamp: Date? {
-        guard let d = lastData?.dateUTC else {
-            return nil
-        }
-        return Date(timeIntervalSince1970: TimeInterval(d / 1000))
-    }
-    
-    /// Return any humidity sensors
-    public var humiditySensors: [AmbientWeatherSensor] {
-        return lastData!.HumiditySensors
-    }
-    
-    /// Return any battery sensors
-    public var batterySensors: [AmbientWeatherSensor] {
-        return lastData!.BatterySensors
-    }
-    
-    /// Return any miscellaneous sensors
-    public var miscellaneousSensors: [AmbientWeatherSensor] {
-        return lastData!.MiscSensors
-    }
-    
-    /// Return any pressure sensors
-    public var pressureSensors: [AmbientWeatherSensor] {
-        return lastData!.PressureSensors
-    }
-    
-    /// Return any rain sensors
-    public var rainSensors: [AmbientWeatherSensor] {
-        return lastData!.RainSensors
-    }
-    
-    /// Return any relay sensors
-    public var relaySensors: [AmbientWeatherSensor] {
-        return lastData!.RelaySensors
-    }
-    
-    /// Return any temperature sensors
-    public var temperatureSensors: [AmbientWeatherSensor] {
-        return lastData!.TemperatureSensors
-    }
-    
-    /// Return any wind sensors
-    public var windSensors: [AmbientWeatherSensor] {
-        return lastData!.WindSensors
-    }
-    
-    /// Return any air quality sensors
-    public var airQualitySensors: [AmbientWeatherSensor] {
-        return lastData!.AirQualitySensors
-    }
-    
     /// Return the MAC Address of the device as reported by AmbientWeather.net
     public var ID: WeatherDeviceID {
         return macAddress
@@ -80,16 +26,6 @@ open class AmbientWeatherDevice: WeatherDevice {
     /// Return the station info.  Note, this returns all info the *user* has entered for the device
     public var information: AmbientWeatherStationInfo? {
         return info
-    }
-    
-    /// Return the station location (if available).
-    public var position: CLLocation? {
-        return info?.geolocation?.position
-    }
-    
-    /// Returns an array containing all sensors that are reporting
-    public var sensors: [AmbientWeatherSensor] {
-        return lastData!.sensors as! [AmbientWeatherSensor]
     }
     
     ///
@@ -108,13 +44,11 @@ open class AmbientWeatherDevice: WeatherDevice {
         
         do {
             macAddress = try container.decodeIfPresent(String.self, forKey: .macAddress) ?? "XXX"
-            lastData = try container.decode(AmbientWeatherStationData.self, forKey: .lastData)
+            //let latestData = try container.decode(AmbientWeatherStationData.self, forKey: .lastData)
             info = try container.decode(AmbientWeatherStationInfo.self, forKey: .info)
         } catch let error as DecodingError {
             throw error
         }
-        
-        //        try super.init(from: decoder)
     }
     
     /// We have to roll our own Codable class due to custom properties
@@ -124,16 +58,9 @@ open class AmbientWeatherDevice: WeatherDevice {
     ///
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        do {
-            try container.encode(macAddress, forKey: .macAddress)
-            try container.encode(lastData, forKey: .lastData)
-            try container.encode(info, forKey: .info)
-        } catch let error as EncodingError {
-            throw error
-        }
-        
-        //        try super.encode(to: encoder)
+
+        try container.encode(macAddress, forKey: .macAddress)
+        try container.encode(info, forKey: .info)
     }
 }
 
