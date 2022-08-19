a1fb7e9116b7494b31f31066e8b82187e02c1c08
Wade Tregaskis <wadetregaskis@mac.com>
2022-08-19 08:46:42 -0700

    Removed unnecessary manual implementation of Codable conformance from AmbientWeatherStationCoordinates.

--- Sources/SwiftWeather/AmbientWeather/AmbientWeatherStationCoordinates.swift
+++ Sources/SwiftWeather/AmbientWeather/AmbientWeatherStationCoordinates.swift
@@ -20,27 +20,4 @@ public class AmbientWeatherStationCoordinates: Codable {
             return nil
         }
     }
-    
-    required public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        do {
-            latitude = try container.decodeIfPresent(Double.self, forKey: .latitude)
-            longitude = try container.decodeIfPresent(Double.self, forKey: .longitude)
-        } catch let error as DecodingError {
-            throw error
-        }
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        do {
-            try container.encode(latitude, forKey: .latitude)
-            try container.encode(longitude, forKey: .longitude)
-        } catch let error as EncodingError {
-            throw error
-        }
-    }
-    
 }
