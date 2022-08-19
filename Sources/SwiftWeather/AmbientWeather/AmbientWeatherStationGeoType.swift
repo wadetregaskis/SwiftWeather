f81e7b6a07af54e4ae93ec860ce2a4a87d0be33a
Wade Tregaskis <wadetregaskis@mac.com>
2022-08-19 08:43:37 -0700

    Removed unnecessary manual conformance to Codable, from AmbientWeatherStationGeoType.
    This also changes its behaviour to _require_ that the JSON contain the coordinates & their type, otherwise decoding will fail.

--- Sources/SwiftWeather/AmbientWeather/AmbientWeatherStationGeoType.swift
+++ Sources/SwiftWeather/AmbientWeather/AmbientWeatherStationGeoType.swift
@@ -1,20 +1,14 @@
 //  Created by Mike Manzo on 5/10/20.
 
-import Foundation
 import CoreLocation
 
 public class AmbientWeatherStationGeoType: Codable {
     private let coordinates: [Double]
     let type: String
-    
-    enum CodingKeys: String, CodingKey {
-        case type = "type"
-        case coordinates = "coordinates"
-    }
-    
+
     /// If the data is present, return a CLLocation object from the reporting lat/lon
     var latLon: CLLocation? {
-        guard coordinates[0] != -1000.0 && coordinates[1] != -1000 else {
+        guard 2 <= coordinates.count else {
             return nil
         }
 
@@ -26,27 +20,6 @@ public class AmbientWeatherStationGeoType: Codable {
         guard let latLon else { return nil }
 
         return latLon.coordinate
-    }
-
-    required public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        do {
-            type = try container.decodeIfPresent(String.self, forKey: .type) ?? "XXX"
-            coordinates = try container.decodeIfPresent([Double].self, forKey: .coordinates) ?? [-1000.0]
-        } catch let error as DecodingError {
-            throw error
-        }
-    }
-    
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        do {
-            try container.encode(type, forKey: .type)
-            try container.encode(coordinates, forKey: .coordinates)
-        } catch let error as EncodingError {
-            throw error
-        }
     }
 }
 
