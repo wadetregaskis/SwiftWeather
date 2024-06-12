a7dbc4b74da54406fdabed3078f1bb74ef6c7676
Wade Tregaskis <wadetregaskis@mac.com>
2024-06-12 13:47:00 -0700

    Removed unnecessary comments.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -11,7 +10,6 @@ let package = Package(
         .tvOS(.v15)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftWeather",
             targets: ["SwiftWeather"]),
@@ -20,8 +18,6 @@ let package = Package(
         //.package(url: "https://github.com/AsyncSwift/AsyncLocationKit.git", from: "1.0.5")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftWeather",
             dependencies: [/* "AsyncLocationKit" */]),
